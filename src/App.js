1cd086b869199efc81563e279d22bc9652e7b4a9
duynghia-123 <duynghia22302@gmail.com>
2022-11-24 09:26:26 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <code>
+          Hello Everyone. My name is Duy Nghia
+          <p>I'm from VKU</p>
+        </code>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
